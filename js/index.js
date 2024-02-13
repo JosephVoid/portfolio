cf275f0c876dffdc84bb3ae9652c3f9bded075d0
Yoseph <yosephten@gmail.com>
2024-02-13 19:41:33 +0300

    `feat`: UI change #4

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+import json from "./data.json";
+
 /* Dragging functionalities */
 $(function () {
   $("#drg-element").dragZoom({
     scope: $("body"),
-    minzoom: 0.5,
+    minzoom: 0.7,
     maxzoom: 2,
     speed: 0.2,
   });
@@ -52,6 +54,62 @@ scale_tags(
     document.getElementsByTagName("p"),
     document.getElementsByTagName("h1"),
     document.getElementsByTagName("h3"),
+    document.getElementsByTagName("i"),
   ],
-  [2.3, 0.5, 1]
+  [2.3, 0.5, 1, 2.5]
 ).observe(document.getElementById("drg-element"));
+
+/* Slide Logic */
+let changeSlide1 = slide("slide_1");
+setInterval(() => changeSlide1(), 5000);
+
+function slide(className) {
+  var prev_works = document.getElementsByClassName(className);
+  var overlay = document.getElementsByClassName("overlay");
+
+  let slides = {
+    turned: false,
+  };
+
+  function overlayOnOff(stat) {
+    for (let index = 0; index < overlay.length; index++) {
+      const overayElement = overlay[index];
+      overayElement.style.opacity = stat;
+    }
+  }
+
+  return function change() {
+    overlayOnOff(1);
+    setTimeout(() => {
+      if (!slides.turned) {
+        prev_works[0].style.display = "none";
+        slides.turned = true;
+      } else {
+        prev_works[0].style.display = "block";
+        slides.turned = false;
+      }
+      overlayOnOff(0);
+    }, 400);
+  };
+}
+
+document.getElementById("expandPW1").addEventListener("click", () => {
+  openModal("previousWork1");
+});
+document.getElementById("closeModalId").addEventListener("click", () => {
+  closeModal();
+});
+
+function openModal(pw) {
+  let modal = document.querySelector("dialog");
+  let modalImg = document.getElementById("modalImg");
+  let figCap = document.getElementById("figCap");
+  modalImg.setAttribute("src", json[pw].img);
+  figCap.innerText = json[pw].text;
+  modal.showModal();
+}
+
+function closeModal() {
+  let modal = document.querySelector("dialog");
+  modal.close();
+}
